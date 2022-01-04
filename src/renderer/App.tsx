0a03e605fb9c76b69c1beb3e449f5b5dce93cb13
bischoff_m <bischoff_m@aim.com>
2022-01-04 03:11:31 +0100

    tested react-zoom-pan-pinch library for canvas

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,19 +1,42 @@
 import Node from './Node';
 import theme from './styles/theme';
-import './styles/App.css';
 
-import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+const useStyles = makeStyles(() => ({
+  container: {
+    'height': '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+  panWrapper: {
+    'width': '100vw',
+    'height': '100vh',
+    backgroundColor: theme.palette.background.default,
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
   return (
-    <Container className='canvas'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        <Node></Node>
-      </ThemeProvider>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <div className={classes.container}>
+        <TransformWrapper
+          limitToBounds={false}
+          minScale={0.5}
+          maxScale={3}
+        >
+          <TransformComponent>
+            <div className={classes.panWrapper}>
+            <Node></Node>
+            </div>
+          </TransformComponent>
+        </TransformWrapper>
+      </div>
+    </ThemeProvider>
   )
 }
