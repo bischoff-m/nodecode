7240eaabfd3f7cfa4308a52a4868312c82a6f884
bischoff_m <bischoff_m@aim.com>
2022-01-04 20:38:13 +0100

    tested own draggable component approach

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,6 @@
 import Node from './Node';
 import theme from './styles/theme';
+import { DragEvent, MouseEvent } from 'react';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
@@ -13,45 +14,60 @@ const useStyles = makeStyles(() => ({
     'width': '100%',
     'height': '100%',
     backgroundColor: theme.palette.background.default,
-    '& > div': {
-      'width': '100vw',
-      'height': '100vh',
-    }
   },
+  draggable: {
+    'width': '100vw',
+    'height': '100vh',
+    transform: 'matrix(1, 0, 0, 1, 20, 0)',
+    backgroundColor: 'red'
+  }
 }));
+var isDragging = false;
 
 export default function App() {
   const panContainer = useRef(null);
   const [num, setNum] = useState(1);
+  const [origin, setOrigin] = useState({ x: 50, y: 50 })
+
+  const classes = useStyles();
 
   // TODO: replace library with own script similar to:
   // panzoom/lib/domController/makeDomController/applyTransform
+  // - listener 'onMouseDown' -> activate 'onMouseMove' and 'onMouseUp'
+  // - listener 'onMouseMove' -> check if mouse down, then transform
+  // - listener 'onMouseUp' -> deactivate 'onMouseMove'
+  // https://www.npmjs.com/package/direct-styled
   // https://react-dnd.github.io/react-dnd/about
-  useEffect(() => {
-    console.log('Called panzoom.')
-    if (panContainer.current)
-      panzoom(panContainer.current, {
-        zoomDoubleClickSpeed: 1,
-        onDoubleClick: (e) => false,
-        beforeMouseDown: (e) => e.button !== 1,
-        filterKey: () => true,
-      });
-  });
 
-  const classes = useStyles();
+  function handleMouseMove(e: MouseEvent) {
+    e.preventDefault()
+    if (!isDragging)
+      return
+    setOrigin({ x: e.clientX, y: e.clientY })
+  }
+
   return (
     <div style={{ overflow: 'hidden' }}>
       <ThemeProvider theme={theme}>
         <CssBaseline enableColorScheme />
-        <div className={classes.container} ref={panContainer}>
-          <div>
+        <div
+          className={classes.container}
+          ref={panContainer}
+          onMouseDown={(e) => { isDragging = e.button === 1 }}
+          onMouseUp={(e) => { isDragging = false }}
+          onMouseMove={handleMouseMove}
+        >
+          <div
+            className={classes.draggable}
+            style={{ transform: `matrix(1, 0, 0, 1, ${origin.x}, ${origin.y})` }}
+          >
             <button onClick={() => setNum(num + 1)}>Hier</button>
             {
               Array(num).fill(0).map((_, i) => (
                 <Node
                   key={i}
-                  x={Math.floor(Math.random() * 500)}
-                  y={Math.floor(Math.random() * 500)}
+                  x={0}
+                  y={120 * i + 30}
                 ></Node>
               ))
             }
