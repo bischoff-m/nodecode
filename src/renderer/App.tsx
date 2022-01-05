caba2b561cd9bc19f76dc239fd9c6afd149c4028
bischoff_m <bischoff_m@aim.com>
2022-01-05 16:12:03 +0100

    added own implementation of draggable canvas using direct-styled

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,78 +1,38 @@
-import Node from './Node';
+import Node from './components/Node';
+import NodeCanvas from './components/NodeCanvas';
 import theme from './styles/theme';
-import { DragEvent, MouseEvent } from 'react';
-
+import { DragEvent, MouseEvent, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
-import panzoom from 'panzoom';
-import { useState, useEffect, useRef } from "react";
-
 const useStyles = makeStyles(() => ({
-  container: {
-    'width': '100%',
-    'height': '100%',
-    backgroundColor: theme.palette.background.default,
-  },
-  draggable: {
-    'width': '100vw',
-    'height': '100vh',
-    transform: 'matrix(1, 0, 0, 1, 20, 0)',
-    backgroundColor: 'red'
-  }
 }));
-var isDragging = false;
 
 export default function App() {
-  const panContainer = useRef(null);
   const [num, setNum] = useState(1);
-  const [origin, setOrigin] = useState({ x: 50, y: 50 })
 
   const classes = useStyles();
 
-  // TODO: replace library with own script similar to:
-  // panzoom/lib/domController/makeDomController/applyTransform
-  // - listener 'onMouseDown' -> activate 'onMouseMove' and 'onMouseUp'
-  // - listener 'onMouseMove' -> check if mouse down, then transform
-  // - listener 'onMouseUp' -> deactivate 'onMouseMove'
-  // https://www.npmjs.com/package/direct-styled
+  // TODO:
   // https://react-dnd.github.io/react-dnd/about
-
-  function handleMouseMove(e: MouseEvent) {
-    e.preventDefault()
-    if (!isDragging)
-      return
-    setOrigin({ x: e.clientX, y: e.clientY })
-  }
 
   return (
     <div style={{ overflow: 'hidden' }}>
       <ThemeProvider theme={theme}>
         <CssBaseline enableColorScheme />
-        <div
-          className={classes.container}
-          ref={panContainer}
-          onMouseDown={(e) => { isDragging = e.button === 1 }}
-          onMouseUp={(e) => { isDragging = false }}
-          onMouseMove={handleMouseMove}
-        >
-          <div
-            className={classes.draggable}
-            style={{ transform: `matrix(1, 0, 0, 1, ${origin.x}, ${origin.y})` }}
-          >
-            <button onClick={() => setNum(num + 1)}>Hier</button>
-            {
-              Array(num).fill(0).map((_, i) => (
-                <Node
-                  key={i}
-                  x={0}
-                  y={120 * i + 30}
-                ></Node>
-              ))
-            }
-          </div>
-        </div>
+        <NodeCanvas>
+          <button onClick={() => setNum(num + 1)}>Hier</button>
+          {
+            Array(num).fill(0).map((_, i) => (
+              <Node
+                key={i}
+                x={0}
+                y={120 * i + 30}
+              ></Node>
+            ))
+          }
+        </NodeCanvas>
       </ThemeProvider>
     </div>
   )
---
+++ src/renderer/components/NodeCanvas.tsx
@@ -0,0 +1,68 @@
+import { Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { ReactNode, MouseEvent } from 'react';
+
+// https://github.com/everweij/direct-styled
+import { directstyled, useDirectStyle } from '../direct-styled';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    'width': '100%',
+    'height': '100%',
+    backgroundColor: theme.palette.background.default,
+  },
+  draggable: {
+    'width': '100vw',
+    'height': '100vh',
+    transform: 'matrix(1, 0, 0, 1, 20, 0)',
+    backgroundColor: 'red'
+  }
+}));
+
+var isDragging = false;
+var prevDragPos = { x: 0, y: 0 }
+var canvasOrigin = { x: 0, y: 0 }
+
+type NodeCanvasProps = {
+  children?: ReactNode
+}
+
+export default function NodeCanvas(props: NodeCanvasProps) {
+  const classes = useStyles();
+  const [style, setStyle] = useDirectStyle();
+
+  function handleMouseDown(e: MouseEvent) {
+    isDragging = e.button === 1
+    prevDragPos = { x: e.clientX, y: e.clientY }
+  }
+
+  function handleMouseMove(e: MouseEvent) {
+    e.preventDefault()
+    if (!isDragging)
+      return
+    canvasOrigin.x += e.clientX - prevDragPos.x
+    canvasOrigin.y += e.clientY - prevDragPos.y
+    prevDragPos.x = e.clientX
+    prevDragPos.y = e.clientY
+    setStyle({
+      transform: `matrix(1, 0, 0, 1, ${canvasOrigin.x}, ${canvasOrigin.y})`,
+    })
+  }
+
+  return (
+    <div
+      className={classes.container}
+      onMouseDown={handleMouseDown}
+      onMouseUp={(e) => { isDragging = false }}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={(e) => { if (e.buttons !== 4) isDragging = false }}
+    >
+      <directstyled.div
+        className={classes.draggable}
+        style={style}
+      >
+        {props.children}
+      </directstyled.div>
+    </div>
+  )
+}
