a33cf1bb0e3d9b481126240cc342ef9368444288
bischoff_m <bischoff_m@aim.com>
2022-04-06 20:18:29 +0200

    updated dependencies and added WebSocket example

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,6 +5,23 @@ import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import store from '@/redux/store';
 
+
+
+
+// import WebSocket from 'ws';
+// console.log(WebSocket);
+
+// // from https://www.npmjs.com/package/ws - Simple Server
+// const wss = new WebSocket.Server({ port: 8080 });
+
+// wss.on('connection', (ws) => {
+//   ws.on('message', (data) => {
+//     console.log('received: %s', data);
+//   });
+
+//   ws.send('something');
+// });
+
 export default function App() {
   return (
     <div style={{ overflow: 'hidden' }}>
