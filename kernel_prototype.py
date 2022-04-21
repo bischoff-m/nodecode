00d7eb5712d2debf8ff5e70df9059afdf26c1770
bischoff_m <bischoff_m@aim.com>
2022-04-22 01:03:38 +0200

    added prototype of async socket.io client

--- kernel_prototype.py
+++ kernel_prototype.py
@@ -1,23 +1,45 @@
-# %%
 
-import json
-from pyknotflow.core import NodeProgram
-import pyknotflow.nodes as nodes
+# import json
+# from pyknotflow.core import NodeProgram
+# import pyknotflow.nodes as nodes
 
-nodes.register_all()
+# nodes.register_all()
 
-with open('./config/programs/example_program.json', 'r') as f:
-    programDef = json.load(f)
+# with open('./config/programs/example_program.json', 'r') as f:
+#     programDef = json.load(f)
 
-program = NodeProgram(programDef)
-output = program.run()
+# program = NodeProgram(programDef)
+# output = program.run()
 
-for outputID, data in output.items():
-    print(f'Output Node {outputID}:')
-    print(data)
+# for outputID, data in output.items():
+#     print(f'Output Node {outputID}:')
+#     print(data)
 
-# %%
+import asyncio
+import socketio
+
+sio = socketio.AsyncClient()
 
 
+@sio.event
+async def connect():
+    print('connection established')
 
-# %%
+
+@sio.event
+async def my_message(data):
+    print('message received with ', data)
+    await sio.emit('my response', {'response': 'my response'})
+
+
+@sio.event
+async def disconnect():
+    print('disconnected from server')
+
+
+async def main():
+    await sio.connect('http://localhost:5000')
+    await sio.wait()
+
+if __name__ == '__main__':
+    asyncio.run(main())
