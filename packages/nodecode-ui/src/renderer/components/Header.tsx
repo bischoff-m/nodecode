338a685fb339f1fe4f47cf7071ee303dc62f6ab1
bischoff_m <bischoff_m@aim.com>
2022-10-12 01:04:06 +0200

    Add documentation to `Header` and `Navbar` component

--- packages/nodecode-ui/src/renderer/components/Header.tsx
+++ packages/nodecode-ui/src/renderer/components/Header.tsx
@@ -1,3 +1,20 @@
+/**
+ * This component is passed to the header prop of the
+ * [AppShell](https://mantine.dev/core/app-shell/) mantine component that wraps the whole
+ * app.
+ * @see {@link "renderer/App" App}
+ * 
+ * **Note**
+ * 
+ * > Currently, this header contains buttons to save and run the program and to send a
+ * > `quit` command to the backend. These are currently only included here for testing
+ * > purposes and should still be placed in a convenient location in the UI in the future.
+ * > The header should then replace the Windows program header, have buttons to minimize,
+ * > maximize and close on the right and contain tabs for the open node programs.
+ * 
+ * @module
+ */
+
 import {
   Header as MantineHeader,
   Button,
@@ -6,6 +23,9 @@ import {
 import SaveFileButton from '@/components/SaveFileButton'
 import backend from '@/util/backendInterface'
 
+/**
+ * @category Component
+ */
 export default function Header() {
   return (
     <MantineHeader height={60} p='sm'>
@@ -16,8 +36,7 @@ export default function Header() {
           // TODO: Revise backend access method and improve the types (omit callback parameter)
           // onClick={() => backend.invoke('run', 'hallo hier mein programm')
           onClick={() => backend
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            .invoke('run', 'hallo hier mein programm', () => { })
+            .invoke('run', 'hallo hier mein programm', () => undefined)
             .then((data) => console.log('antwort ist zurück: ', data))
           }
         >RUN</Button>
--- packages/nodecode-ui/src/renderer/components/Navbar.tsx
+++ packages/nodecode-ui/src/renderer/components/Navbar.tsx
@@ -1,6 +1,24 @@
+/**
+ * This component is passed to the navbar prop of the
+ * [AppShell](https://mantine.dev/core/app-shell/) mantine component that wraps the whole
+ * app.
+ * @see {@link "renderer/App" App}
+ * 
+ * **Note**
+ * 
+ * > Currently, this navbar is empty. In the future, it should contain icon buttons that
+ * > provide access to the application's main functions, such as a file explorer, output
+ * > window, node browser, package browser, program configuration, or other tools.
+ * 
+ * @module
+ */
+
 import { Navbar as MantineNavbar } from '@mantine/core'
 
-export default function Navbar() {
+/**
+ * @category Component
+ */
+export default function Navbar(): JSX.Element {
   return (
     <MantineNavbar width={{ base: 60 }}>
     </MantineNavbar>
