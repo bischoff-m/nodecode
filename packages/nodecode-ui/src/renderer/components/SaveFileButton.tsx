6289c9b83d81d61496a23698f4bbf50876421f73
bischoff_m <bischoff_m@aim.com>
2022-10-24 01:34:53 +0200

    Add documentation to `SaveFileButton` and disable the button if synced

--- packages/nodecode-ui/src/renderer/components/SaveFileButton.tsx
+++ packages/nodecode-ui/src/renderer/components/SaveFileButton.tsx
@@ -1,13 +1,43 @@
+/**
+ * This button writes the current global program state in the
+ * {@link "renderer/redux/programSlice" programSlice} to the current JSON program file.
+ * The actual writing is done by the main process, so when clicked, the button will send
+ * a request via the IPC. It also disables itself when clicked and gets enabled when
+ * the program state changed.
+ * 
+ * @see {@link "renderer/components/Header" Header}
+ * 
+ * **Note**
+ * 
+ * > Currently the *Save* feature only supports a single file linked to a single program
+ * > in state. In the future, there should be a file explorer to open and save files and
+ * > the possibility to have multiple programs open. For this, the global program state in
+ * > the `programSlice` has to be revised to support multiple programs.
+ * 
+ * @module
+ */
+
 import { Button } from '@mantine/core'
+import { useEffect, useState } from 'react'
 import { useSelectorTyped } from '@/redux/hooks'
 
-export default function SaveFileButton() {
+/** @category Component */
+export default function SaveFileButton(): JSX.Element {
   const program = useSelectorTyped((state) => state.program)
+  const [disabled, setDisabled] = useState<boolean>(false)
+
+  useEffect(() => {
+    setDisabled(false)
+  }, [program])
 
   return (
     <Button
-      color="primary"
-      onClick={() => window.ipc.send.saveProgram(program)}
+      disabled={disabled}
+      color='primary'
+      onClick={() => {
+        window.ipc.send.saveProgram(program)
+        setDisabled(true)
+      }}
     >SAVE</Button>
   )
 }
