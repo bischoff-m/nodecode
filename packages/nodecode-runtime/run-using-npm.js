027ce9620f6199e9ae4b8a4eaba87967e7a6bb6b
Marco Bischoff <bischoff_m@aim.com>
2023-06-04 16:54:14 +0200

    Add comment and disable spell checking

--- packages/nodecode-runtime/run-using-npm.js
+++ packages/nodecode-runtime/run-using-npm.js
@@ -1,5 +1,8 @@
 // TODO
 
+// currently, nodecode-runtime is run directly from the command line
+// this is not ideal, so this script should be implemented to run it from npm
+
 // check if python & pip are installed, else throw error and open browser
 // check if pipenv is installed, else ask if should be installed
 
